4ff933bc78137d6e5b037dddea32d8e2ab60a579
Rebecca Poulson <rebeccapoulson@knightlabs-MacBook-Pro.local>
2016-07-05 15:54:14 -0500

    load scripts and html template

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
-$('.buttonBookmark').click(function(e) {
-  createiFrame(e);
-})
+$(document).ready(function(){
+	$('.buttonBookmark').click(function(e) {
+	  loadTemplate(e);
+	})
 
-$('.closeMe').click(function(e) {
-  e.preventDefault();
-  $('.bookmarklet').remove();
-})
+	$('.closeMe').click(function(e) {
+	  e.preventDefault();
+	  $('.bookmarklet').remove();
+	})
 
-function createiFrame(e) {
-  e.preventDefault();
-  var element = document.createElement('iframe');
-  $('body').append(element);
-  $('iframe').attr('src', 'http://www.shortdiv.com');
-}
+	function createiFrame(e) {
+	  e.preventDefault();
+	  var element = document.createElement('iframe');
+	  $('body').append(element);
+	  $('iframe').attr('src', 'http://www.shortdiv.com');
+	}
+
+	function loadTemplate(e){
+	  e.preventDefault();
+	  var element = document.createElement('div');
+	  $('body').append(element);
+	  $(element).load('/template.html', function(){closeIframe(e)});
+	}
+
+	function closeIframe(e) {
+	  $('.closeMe').on('click', function(e) {
+		  e.preventDefault();
+		  $('.bookmarklet').remove();
+		});
+	};
+});
