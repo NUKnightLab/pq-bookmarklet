94d37e6b387b6f7b7b7b0bda7cb76f7cb99fc24f
Divya Sasidharan <hello@shortdiv.com>
2016-07-08 14:18:28 -0500

    fix: remove unused variables

---
+++ js/refactor-script.js
@@ -0,0 +1,18 @@
+function iFrame() {
+  function init() {
+    bookMarklet = document.createElement('div');
+    bookMarklet.className = 'bookMarklet';
+    bookMarklet.innerHTML = "<button class='closeButton' onclick='iFrame().closeiFrame()'></button><iframe src='/template.html'></iframe><div class='backdrop'></div>"
+
+    document.body.appendChild(bookMarklet);
+  }
+
+  function closeiFrame() {
+    document.body.removeChild(bookMarklet)
+  }
+
+  return {
+    init: init,
+    closeiFrame: closeiFrame
+  }
+}
